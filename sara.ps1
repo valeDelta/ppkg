b751ba0734a5bba719a47598deecd7a8cb5ae83d
valeDelta <valerio.evarchi@deltainformatica.eu>
2025-01-17 17:32:16 +0100

    aggiunta controllo sull'esistenza di cartella sara e file di update

--- sara.ps1
+++ sara.ps1
@@ -1,8 +1,16 @@
-﻿Set-Location C:\temp\SaRAcmd
+﻿if(Get-ChildItem -Path "C:\temp" -Filter "SaRAcmd"){
+    Set-Location C:\temp\SaRAcmd
 
-If ((Get-NetConnectionProfile).IPv4Connectivity -contains "Internet" -or (Get-NetConnectionProfile).IPv6Connectivity -contains "Internet") {
-     .\SaRAcmd -S OfficeScrubScenario -AcceptEula -OfficeVersion All
+    If ((Get-NetConnectionProfile).IPv4Connectivity -contains "Internet" -or (Get-NetConnectionProfile).IPv6Connectivity -contains "Internet") {
+        .\SaRAcmd -S OfficeScrubScenario -AcceptEula -OfficeVersion All
+   }
+   else{
+       exit
+   }
 }
 else{
     exit
 }
+
+
+
--- start.ps1
+++ start.ps1
@@ -1,14 +1,19 @@
 ﻿#'{0}:\' -f (Get-VOlume | Where-Object DriveType -eq 'Removable').DriveLetter | Set-Location
+if (Get-ChildItem -Path "C:\temp" -Filter "update.ps1") { 
+    # creo le azioni da eseguire
+    Set-ExecutionPolicy Unrestricted -Scope LocalMachine
+    #$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "D:\prova.ps1 -WindowStyle Maximized"
+    $action1 = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "C:\temp\update.ps1 -WindowStyle Maximized"
 
-# creo le azioni da eseguire
-Set-ExecutionPolicy Unrestricted -Scope LocalMachine
-#$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "D:\prova.ps1 -WindowStyle Maximized"
-$action1 = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "C:\temp\update.ps1 -WindowStyle Maximized"
+    #creo il trigger e impostazioni per l'attività
+    $trigger = New-ScheduledTaskTrigger -AtLogOn
+    $task = New-ScheduledTaskPrincipal -RunLevel Highest -UserId Utente
 
-#creo il trigger e impostazioni per l'attività
-$trigger = New-ScheduledTaskTrigger -AtLogOn
-$task = New-ScheduledTaskPrincipal -RunLevel Highest -UserId Utente
+    # registro le attività
+    #Register-ScheduledTask -Action $action -Trigger $trigger -Principal $task -TaskName "continue"
+    Register-ScheduledTask -Action $action1 -Trigger $trigger -Principal $task -TaskName "update"
+} 
+else { 
+    exit
+}
 
-# registro le attività
-#Register-ScheduledTask -Action $action -Trigger $trigger -Principal $task -TaskName "continue"
-Register-ScheduledTask -Action $action1 -Trigger $trigger -Principal $task -TaskName "update"
