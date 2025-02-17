f963199352c0a3f5f419e1ad3352c9e9ecf68e02
valeDelta <valerio.evarchi@deltainformatica.eu>
2025-02-17 15:43:45 +0100

    minor fix su lingua e formato regionale

--- config.ps1
+++ config.ps1
@@ -2,7 +2,6 @@ Write-Warning "Prima di eseguire lo script controllare che il PC sia collegato a
 pause
 
 #da rimuovere nella prossima versione del ppkg 1.5
-Set-TimeZone -id "W. Europe Standard Time"
 Set-WinUserLanguageList it-IT -force
 Set-WinHomeLocation -GeoId 118
 Set-Culture it-IT
