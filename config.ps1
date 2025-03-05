829c5468f26e4c652f8f2bda7b2d97eaa203d41c
valeDelta <valerio.evarchi@deltainformatica.eu>
2025-03-05 11:27:51 +0100

    aggiunta utente personalizzato e rimozione di quello senza pwd

--- config.ps1
+++ config.ps1
@@ -7,20 +7,31 @@ $PD = "C:\Users\Public\Desktop"
 Unregister-ScheduledTask -TaskName "continue" -Confirm:$False
 
 
-
+function remove {
+    param (
+        $username
+    )
+    $action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "Unregister-ScheduledTask -TaskName 'remove' -Confirm:$False
+    remove-localuser -Name '$username' -confirm:$false"
+    $trigger = New-ScheduledTaskTrigger -AtLogOn -User "$username"
+    $task = New-ScheduledTaskPrincipal -RunLevel Highest -UserId $username
+    Register-ScheduledTask -Action $action -Trigger $trigger -Principal $task -TaskName "remove"
+}
 function set-delta {
     $username = "DeltaAdmin"
     $pass = Read-Host "Inserire la password per l'utente DeltaAdmin"
     $password = ConvertTo-SecureString $pass -AsPlainText -Force 
-    New-LocalUser -Name $username -Password $password -FullName $username -AccountExpires -PasswordNeverExpires
+    New-LocalUser -Name $username -Password $password -FullName $username -AccountNeverExpires -PasswordNeverExpires
     Add-LocalGroupMember -Group "Administrators" -Member $username
+    remove($username)   
 }
 function set-saidea {
-    $username = "Sadiea"
+    $username = "Saidea"
     $pass = Read-Host "Inserire la password per l'utente Saidea"
     $password = ConvertTo-SecureString $pass -AsPlainText -Force 
-    New-LocalUser -Name $username -Password $password -FullName $username -AccountExpires -PasswordNeverExpires
+    New-LocalUser -Name $username -Password $password -FullName $username -AccountNeverExpires -PasswordNeverExpires
     Add-LocalGroupMember -Group "Administrators" -Member $username
+    remove($username)
 }
 
 $client = Read-Host "
