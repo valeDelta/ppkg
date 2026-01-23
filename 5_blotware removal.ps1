1487522b8e33d6ea259003473f406682866935a5
valeDelta <valerio.evarchi@deltainformatica.eu>
2026-01-23 15:37:19 +0100

    implementata automatica connessione al wifi e file di rimozzione blootware

---
+++ 5_blotware removal.ps1
@@ -0,0 +1,23 @@
+﻿Get-AppxPackage -all Microsoft.YourPhone | Remove-AppxPackage -AllUsers
+Get-AppxPackage -all Microsoft.Xbox.TCUI | Remove-AppxPackage -AllUsers
+Get-AppxPackage -all Microsoft.MicrosoftOfficeHub | Remove-AppxPackage -AllUsers
+Get-AppxPackage -all MicrosoftCorporationII.MicrosoftFamily | Remove-AppxPackage -AllUsers
+Get-AppxPackage -all Microsoft.XboxGamingOverlay | Remove-AppxPackage -AllUsers
+Get-AppxPackage -all Microsoft.Todos | Remove-AppxPackage -AllUsers
+Get-AppxPackage -all .Clipchamp.Clipchamp | Remove-AppxPackage -AllUsers
+Get-AppxPackage -all Microsoft.PowerAutomateDesktop | Remove-AppxPackage -AllUsers
+Get-AppxPackage -all Microsoft.GamingApp | Remove-AppxPackage -AllUsers
+Get-AppxPackage -all Microsoft.XboxSpeechToTextOverlay | Remove-AppxPackage -AllUsers
+Get-AppxPackage -all Microsoft.Windows.DevHome | Remove-AppxPackage -AllUsers
+Get-AppxPackage -all MSTeams | Remove-AppxPackage -AllUsers
+Get-AppxPackage -all Microsoft.OutlookForWindows | Remove-AppxPackage -AllUsers
+Get-AppxPackage -all Dell.SupportAssistforPCs | Remove-AppxPackage -AllUsers
+Get-AppxPackage -all McAfeeWPSSparsePackage | Remove-AppxPackage -AllUsers
+
+
+#disinstall mcafee
+Set-Location "C:\Program Files\McAfee\WebAdvisor"
+.\uninstaller.exe /s
+Set-Location "C:\Program Files\McAfee"
+Set-Location "C:\Program Files\McAfee\wps\1.*"
+.\mc-update.exe -uninstall /silent /force
