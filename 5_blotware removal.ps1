6a2f82f1c295a5e40d82e7c5d18d1dd0473cac32
valeDelta <valerio.evarchi@deltainformatica.eu>
2026-02-13 11:16:06 +0100

    aggiunto app da rimuovere

--- 5_blotware removal.ps1
+++ 5_blotware removal.ps1
@@ -4,15 +4,18 @@ Get-AppxPackage -all Microsoft.MicrosoftOfficeHub | Remove-AppxPackage -AllUsers
 Get-AppxPackage -all MicrosoftCorporationII.MicrosoftFamily | Remove-AppxPackage -AllUsers
 Get-AppxPackage -all Microsoft.XboxGamingOverlay | Remove-AppxPackage -AllUsers
 Get-AppxPackage -all Microsoft.Todos | Remove-AppxPackage -AllUsers
-Get-AppxPackage -all .Clipchamp.Clipchamp | Remove-AppxPackage -AllUsers
+Get-AppxPackage -all Clipchamp.Clipchamp | Remove-AppxPackage -AllUsers
 Get-AppxPackage -all Microsoft.PowerAutomateDesktop | Remove-AppxPackage -AllUsers
 Get-AppxPackage -all Microsoft.GamingApp | Remove-AppxPackage -AllUsers
 Get-AppxPackage -all Microsoft.XboxSpeechToTextOverlay | Remove-AppxPackage -AllUsers
 Get-AppxPackage -all Microsoft.Windows.DevHome | Remove-AppxPackage -AllUsers
+Get-AppxPackage -all Microsoft.XboxIdentityProvider | Remove-AppxPackage -AllUsers
 Get-AppxPackage -all MSTeams | Remove-AppxPackage -AllUsers
 Get-AppxPackage -all Microsoft.OutlookForWindows | Remove-AppxPackage -AllUsers
 Get-AppxPackage -all Dell.SupportAssistforPCs | Remove-AppxPackage -AllUsers
 Get-AppxPackage -all McAfeeWPSSparsePackage | Remove-AppxPackage -AllUsers
+Get-AppxPackage -all Microsoft.Copilot| Remove-AppxPackage -AllUsers
+Get-AppxPackage -all Microsoft.XboxGameCallableUI | Remove-AppxPackage -AllUsers
 
 
 #disinstall mcafee
